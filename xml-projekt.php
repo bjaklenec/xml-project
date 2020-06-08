5f875cc9de64bfe17695c4695e7a16d311acf207
Borna Jaklenec <66021754+bjaklenec@users.noreply.github.com>
2020-06-08 14:41:12 +0200

    style.css

--- xml-projekt.php
+++ xml-projekt.php
@@ -45,7 +45,7 @@ file_put_contents($filename, $file);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style6.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600&display=swap" rel="stylesheet">
     <title>XML | L'Obs</title>
